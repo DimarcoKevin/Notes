99aa029d2250d0920028b998fd5d12163ebff69c
Kevin <kevin.j.dimarco@gmail.com>
2020-07-27 00:46:18 -0700

    Adding create table statement and databaseHelper class

--- app/src/main/java/com/dimarco/notes/DbManager.kt
+++ app/src/main/java/com/dimarco/notes/DbManager.kt
@@ -1,15 +1,36 @@
 package com.dimarco.notes
 
+import android.content.Context
+import android.database.sqlite.SQLiteAbortException
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
 class DbManager {
 
     val dbVersion = 1
-    val dbNamed = "MyNotes"
+    val dbName = "MyNotes"
     val dbTable = "Notes"
     val colID = "ID"
     val colTitle = "Title"
     val colContent = "Content"
 
-    
+    val sqlDB: SQLiteDatabase? = null
+    val sqlCreateTable = "CREATE TABLE IF NOT EXIST $dbTable ($colID INTEGER PRIMARY KEY, $colTitle VARCHAR(255), $colContent VARCHAR(1024));"
 
     constructor()
+
+
+    inner class DatabaseHelperNotes(context: Context) :
+        SQLiteOpenHelper(context, dbName, null, dbVersion) {
+
+
+        override fun onCreate(db: SQLiteDatabase?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
 }
