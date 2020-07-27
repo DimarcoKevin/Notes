40d0ff971cda63b88eda4cf033922282159e54cf
Kevin <kevin.j.dimarco@gmail.com>
2020-07-27 00:37:16 -0700

    Adding database manager class

---
+++ app/src/main/java/com/dimarco/notes/DbManager.kt
@@ -0,0 +1,15 @@
+package com.dimarco.notes
+
+class DbManager {
+
+    val dbVersion = 1
+    val dbNamed = "MyNotes"
+    val dbTable = "Notes"
+    val colID = "ID"
+    val colTitle = "Title"
+    val colContent = "Content"
+
+    
+
+    constructor()
+}
